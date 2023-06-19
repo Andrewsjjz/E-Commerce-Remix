40a2deba0c1e134a59c14da2c2df3362f577f9e0
Andres Jimenez <andresjimenez884@gmail.com>
2023-06-19 15:58:45 -0400

    cambios en consultar api

--- app/api/blog.server.js
+++ app/api/blog.server.js
@@ -1,11 +1,11 @@
 export async function getBlog () {
-    const respuesta= await fetch(`${process.env.API_URL}/informacion-generals?populate=imagen`)
+    const respuesta= await fetch(`${process.env.API_URL}/api/informacion-generals?populate=imagen`)
     const resultado= await respuesta.json()
     return resultado
 }   
 
 export async function getBlogId (url) {
-    const respuesta= await fetch(`${process.env.API_URL}/informacion-generals?filters[url]=${url}&populate=imagen`)
+    const respuesta= await fetch(`${process.env.API_URL}/api/informacion-generals?filters[url]=${url}&populate=imagen`)
     const resultado= await respuesta.json()
     return resultado
 }  
--- app/api/gaming.server.js
+++ app/api/gaming.server.js
@@ -1,11 +1,11 @@
 export async function getGaming () {
-    const respuesta= await fetch(`http://127.0.0.1:1337/api/gamers?populate=imagen`)
+    const respuesta= await fetch(`${process.env.API_URL}/api/gamers?populate=imagen`)
     const resultado= await respuesta.json()
     return resultado
 }   
 
 export async function GetGamingId(url) {
-    const respuesta = await fetch(`http://127.0.0.1:1337/api/gamers?filters[url]=${url}&populate=imagen&`)
+    const respuesta = await fetch(`${process.env.API_URL}/api/gamers?filters[url]=${url}&populate=imagen&`)
     const resultado = await respuesta.json()
     return resultado
 }
